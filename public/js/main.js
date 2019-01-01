4ccbaaa9d7d935ef39f9255a405ef225b911e23d
Benjamin Howe <ben@bh96.uk>
2019-01-01 21:58:14 +0000

    Added lots of boilerplate, plus some logic around providers

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+function addCard() {
+	swal({
+		"title": "Gift Card Brand",
+		"input": "select",
+		"inputOptions": getProvidersForSWAL(),
+	});
+}
+
+function displayGiftCards() {
+	// if no giftcards are defined, don't do anything
+	if (!giftcards) {
+		return;
+	}
+
+	// TODO
+}
+
+function getProvidersForSWAL() {
+	let providersForSWAL = new Object();
+	Object.keys(providers).forEach(function(key) {
+		providersForSWAL[key] = providers[key].name;
+	});
+	return providersForSWAL;
+}
+
+function showOptions() {
+	// TODO
+	swal({"title": "TODO"});
+}
+
+function updateProviders() {
+	fetch("/api/providers").then(function(response) {
+		// TODO: detect 429 etc.
+		return response.text();
+	}).then(function(body) {
+		providersAsOf = new Date();
+		providers = JSON.parse(body);
+		localStorage.providersAsOf = providersAsOf;
+		localStorage.providers = body;
+	});
+}
+
+// define variables
+let giftcards = localStorage.giftcards;
+let providers = localStorage.providers;
+let providersAsOf = localStorage.providersAsOf;
+
+// parse data from localStorage
+if (giftcards) {
+	giftcards = JSON.parse(giftcards);
+}
+
+if (providersAsOf) {
+	providersAsOf = new Date(providersAsOf);
+}
+
+if (providers && ((new Date().getTime() - providersAsOf.getTime()) / 1000) < 60 * 60 * 4) {
+	// if providers exist and they're less than 4 hours old
+	providers = JSON.parse(providers);
+} else {
+	updateProviders();
+}
+
+// display gift cards
+displayGiftCards();
+
+// bind event listeners
+document.getElementById("addCard").addEventListener("click", addCard);
+document.getElementById("showOptions").addEventListener("click", showOptions);
