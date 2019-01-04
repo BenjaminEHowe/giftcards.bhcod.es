69f1f37008af0019bd0f8d210317e7fb8f90321c
Benjamin Howe <ben@bh96.uk>
2019-01-04 00:09:27 +0000

    Added more gift card validation, persisted gift cards to localStorage

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 function addCard() {
+	var provider;
 	swal({
 		"title": "Gift Card Brand",
 		"input": "select",
@@ -8,8 +9,41 @@ function addCard() {
 		if(result.dismiss) {
 			return Promise.reject("Cancelled");
 		}
+		return result.value;
+	}).then((result) => {
+		provider = providers[result];
+		provider.slug = result;
+		let swalQuestions = new Array();
+		for(let i = 0; i < provider.requiredData.length; i++) {
+			let question = new Object();
+			question.title = provider.requiredData[i].fullName;
+			question.inputValidator = (value) => {
+				return (!value.match(new RegExp(provider.requiredData[i].validation)) ||
+					!(provider.requiredData[i].slug != "pan" || validateLuhn(value))) &&
+					`That doesn't appear to be a valid ${provider.requiredData[i].name}`;
+			}
+			swalQuestions.push(question);
+		}
 
-		console.log(providers[result.value].requiredData);
+		return swal.mixin({
+			"input": "text",
+			"progressSteps": Array.from({length: swalQuestions.length}, (v, k) => k+1),
+			"showCancelButton": true,
+		}).queue(swalQuestions);
+	}).then((result) => {
+		if(result.dismiss) {
+			return Promise.reject("Cancelled");
+		}
+		return result.value;
+	}).then((result) => {
+		let card = new Object();
+		for (let i = 0; i < result.length; i++) {
+			card[provider.requiredData[i].slug] = result[i];
+		}
+		card.slug = provider.slug;
+		giftcards["Default"].push(card);
+		localStorage.giftcards = JSON.stringify(giftcards);
+		displayGiftCards();
 	}).catch(function(error) {
 		switch(error) {
 			case "Cancelled":
@@ -28,7 +62,25 @@ function displayGiftCards() {
 		return;
 	}
 
-	// TODO
+	let table = document.createElement("table");
+	table.id = "giftcardTable";
+	tbody = document.createElement("tbody");
+	for (let i = 0; i < giftcards["Default"].length; i++) {
+		let tr = document.createElement("tr");
+		{
+			let td = document.createElement("td");
+			let logo = document.createElement("img");
+			logo.src = providers[giftcards["Default"][i].slug].logo;
+			logo.alt = `${providers[giftcards["Default"][i].slug].name} logo`;
+			td.appendChild(logo);
+			tr.appendChild(td);
+		}
+		// TODO: name and balance
+		tbody.appendChild(tr);
+	}
+	table.appendChild(tbody);
+	document.getElementById("mainContainer").innerHTML = "";
+	document.getElementById("mainContainer").appendChild(table);
 }
 
 function getProvidersForSWAL() {
@@ -56,6 +108,25 @@ function updateProviders() {
 	});
 }
 
+function validateLuhn(value) {
+	// source: https://gist.github.com/DiegoSalazar/4075533
+	// accept only digits, dashes or spaces
+	if (/[^0-9-\s]+/.test(value)) return false;
+	// The Luhn Algorithm. It's so pretty.
+	var nCheck = 0, nDigit = 0, bEven = false;
+	value = value.replace(/\D/g, "");
+	for (var n = value.length - 1; n >= 0; n--) {
+		var cDigit = value.charAt(n),
+			nDigit = parseInt(cDigit, 10);
+		if (bEven) {
+			if ((nDigit *= 2) > 9) nDigit -= 9;
+		}
+		nCheck += nDigit;
+		bEven = !bEven;
+	}
+	return (nCheck % 10) == 0;
+};
+
 // define variables
 let giftcards = localStorage.giftcards;
 let providers = localStorage.providers;
@@ -64,6 +135,9 @@ let providersAsOf = localStorage.providersAsOf;
 // parse data from localStorage
 if (giftcards) {
 	giftcards = JSON.parse(giftcards);
+} else {
+	giftcards = new Object();
+	giftcards["Default"] = new Array();
 }
 
 if (providersAsOf) {
